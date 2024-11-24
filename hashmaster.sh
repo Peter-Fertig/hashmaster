3ff8113b7afaf394bd1017af435f545750e9b7f8
pefertig <petree.fertig@gmail.com>
2024-11-23 22:35:16 -0500

    cleaned up the code and added some more color

--- hashmaster.sh
+++ hashmaster.sh
@@ -2,27 +2,31 @@
 #clean up files for re-use
 rm download_list1.txt 2>/dev/null
 rm download_list2.txt 2>/dev/null
+#set text color variables
+RED='\033[0;31m'
+GRN='\033[0;32m'
+MAG='\033[0;35m'
+NC='\033[0m'
+
+#clear the screen
 clear
-#0. get username
-#
+#get username
+
 user_name=`whoami`
 
-echo "welcome $user_name"
-dl_directory="/home/$user_name/Downloads"
-#1. have user paste in checksum from the website
-#
+#have user paste in checksum from the website
+
 echo please paste SHA256 checksum from website:  
 
-#
-#2. save the first checksum as variable sum1
-#
+
+#save the first checksum as variable sum1
+
 read sum1
 
-#sum1=$(cat $dl_directory/sumfile.txt)
-#echo $sum1
-#
-#3. give user a numbered list of files in the dowloads folder for them to choose from
-#
+
+#give user a numbered list of files in the dowloads folder for them to choose from
+
+dl_directory="/home/$user_name/Downloads"
 
 ls $dl_directory -1 >> download_list1.txt
 
@@ -37,19 +41,17 @@ read selection
 var=( $(ls $dl_directory) ) 
 check=${var[$selection-1]}
 echo ""
-echo "checking file integrity of $check"
+echo -e "${MAG}checking file integrity of $check${NC}"
 echo ""
-#4. set variable sum2 to --> run shasum on selected file and remove additional information 
+
+#set variable sum2 to --> run shasum on selected file
 
 sum2=$(shasum -a 256 $dl_directory/$check | awk '{print $1}') 
 #echo $sum2 | awk '{print $1}'
-#
-#
-#5. check if sum1 and sum2 are equal
-#
-RED='\033[0;31m'
-GRN='\033[0;32m'
-NC='\033[0m'
+
+
+#check if sum1 and sum2 are equal
+
 echo "SHA 256 checksum  of  file from website: $sum1"
 echo "SHA 256 checksum of file you downloaded: $sum2"
 echo""
@@ -59,5 +61,3 @@ if [ "$sum1" == "$sum2" ]; then
 else
 	echo -e "${RED}!!!!!!!!!!WARNING!!!!!!!!!!\n${NC}These checksums do not match!\nFile integrity cannot be verified."
 fi
-echo""
-echo""
